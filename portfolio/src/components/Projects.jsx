4129cb761c8877124853539bcb65d8e929debfad
Ajibola <alaomiyaki17@gmail.com>
2025-01-03 10:46:24 +0100

    add a data file and worked on projects.jsx file

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,14 +1,11 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import Project from './Project'
-import devlinks from "../assets/image/devlinks-preview.png"
-import dimplattech from "../assets/image/dimplattech-preview.png"
-import bloggingAPi from "../assets/image/bloggin-api-preview.png"
-import desserts from "../assets/image/desserts-ecommerce-preview.png" 
-import partypal from "../assets/image/partypal-preview.png"
-import thankyoucard from "../assets/image/thank-you-card-preview.png"
-import the4thent from "../assets/image/the4thent-preview.png"
+import { data } from '../data/data'
+
 
 export default forwardRef (function Projects(_,ref) {
+    const [projects, setProjects] = useState(data)
+    console.log( projects)
   return (
     <section ref={ref} className='mb-10 md:mb-12 lg:mb-16 xl:mb-20'>
         <p className='text-center text-2xl lg:text-3xl xl:text-4xl text-black dark:text-white font-bold mb-4'>My Projects</p>
@@ -22,46 +19,21 @@ export default forwardRef (function Projects(_,ref) {
             <li className='cursor-pointer hover:bg-[#fd6d00] hover:text-white py-1 px-2 rounded-lg border border-[#969696] bg-[#f8f8f8]'>Backend</li>
         </ul>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center gap-4'>
-            <Project 
-                image={devlinks}
-                name={"Devlinks"}
-                desc={"A web app for storing and sharing links with preview pages, enabling easy sharing with friends, family, or employers."}
-                stack={["React.js","Typescript","TailwindCss"]}
-                sourceCode={"https://github.com/Jbsmall17/link-sharing-webapp"}
-                liveServer={"https://link-sharing-webapp.vercel.app/"}
-            />
-            <Project 
-                image={dimplattech}
-                name={"dimplattech's website"}
-                desc={"the official website of Dimplattech"}
-                stack={["Next.js","Typescript","TailwindCss"]}
-                sourceCode={"https://github.com/Jbsmall17/dimplattech"}
-                liveServer={"https://dimplattech.vercel.app/"}
-            />
-            <Project 
-                image={partypal}
-                name={"Partypal"}
-                desc={"Partypal is a comprehensive event planning platform that simplifies the process of connecting event planners with vendor"}
-                stack={["React.js","Typescript","TailwindCss"]}
-                sourceCode={"https://github.com/midrohuborg/PartyPal"}
-                liveServer={"https://www.partypalnow.com/"}
-            />
-            <Project 
-                image={bloggingAPi}
-                name={"A bloging API"}
-                desc={"This is a CRUD RESTful API that allows users to read blogs. Only authenticated users can create, update, and delete blogs."}
-                stack={["Express.js","Javascript","MongoDB"]}
-                sourceCode={"https://github.com/Jbsmall17/Blogging_API"}
-                liveServer={"https://blog-api-ljcf.onrender.com/"}
-            />
-            <Project 
-                image={the4thent}
-                name={"4thent's website"}
-                desc={"Official website of 4th entertainment"}
-                stack={["React.js","Javascript","CSS"]}
-                sourceCode={"https://github.com/Jbsmall17/The4thEntWebsite"}
-                liveServer={"https://www.the4thent.com"}
-            />      
+            {
+                projects.map(({name,desc,stack,imagePreview,sourceCode,liveServer},index)=>{
+                    return (
+                        <Project
+                            key={index} 
+                            image={imagePreview}
+                            name={name}
+                            desc={desc}
+                            stack={stack}
+                            sourceCode={sourceCode}
+                            liveServer={liveServer}
+                        />
+                    )
+                })
+            }
         </div>
     </section>
   )
---
+++ portfolio/src/data/data.js
@@ -0,0 +1,66 @@
+export const data = [
+    {
+        name: "Devlinks",
+        desc: "A web app for storing and sharing links with preview pages, enabling easy sharing with friends, family, or employers.",
+        stack: ["React.js","Typescript","TailwindCss","Express.js", "MongoDB"],
+        imagePreview: "/devlinks-preview.png",
+        sourceCode:"https://github.com/Jbsmall17/link-sharing-webapp",
+        liveServer:"https://link-sharing-webapp.vercel.app/",
+        type: "Fullstack"
+    },
+    {
+        name: "Dimplattech",
+        desc: "Dimplattech's website is a multi-page platform with sleek animations and seamless transitions for an engaging user experience.",
+        stack: ["Next.js","Typescript","TailwindCss","Firebase"],
+        imagePreview: "/dimplattech-preview.png",
+        sourceCode: "https://github.com/Jbsmall17/dimplattech",
+        liveServer: "https://dimplattech.vercel.app/",
+        type: "Fullstack"
+    },
+    {
+       name: "Partypal",
+       desc: "Partypal is a comprehensive event planning platform that simplifies the process of connecting event planners with vendor",
+       stack: ["React.js","Typescript","TailwindCss"],
+       imagePreview: "/partypal-preview.png",
+       sourceCode: "https://github.com/Jbsmall17/Blogging_API",
+       liveServer: "https://blog-api-ljcf.onrender.com/",
+       type: "Frontend"
+    },
+    {
+        name: "Blogging RESTful API",
+        desc: "This is a CRUD RESTful API that allows users to read blogs. Only authenticated users can create, update, and delete blogs.",
+        stack: ["Express.js","Javascript","MongoDB"],
+        imagePreview: "bloggin-api-preview.png",
+        sourceCode: "https://github.com/Jbsmall17/Blogging_API",
+        liveServer: "https://blog-api-ljcf.onrender.com/",
+        type: "Backend"
+    },
+    {
+        name: "4thent",
+        desc:"4th Entertainment's website features a visually appealing UI that reflects the brand's identity with sleek design and seamless functionality.",
+        stack: ["React.js","Javascript","CSS"],
+        imagePreview: "/the4thent-preview.png",
+        sourceCode: "https://github.com/Jbsmall17/The4thEntWebsite",
+        liveServer: "https://www.the4thent.com",
+        type: "Frontend"
+    },
+    {
+        name: "Desserts e-commerce",
+        desc: "An e-commerce website for desserts with elegant design, easy navigation, and a seamless shopping experience.",
+        stack: ["React.js, Javascript", "TailwindCss"],
+        imagePreview: "/desserts-ecommerce-preview.png",
+        sourceCode: "https://github.com/Jbsmall17/dessert-app",
+        liveServer: "https://dessert-app-flax.vercel.app/",
+        type: "Frontend"
+    },
+    {
+        name: "Thank-you-card generator",
+        desc: "A web app that generates custom 'Thank You' cards using images from the Unsplash API, allowing users to add personalized text.",
+        stack: ["React.js","Typescript","CSS"],
+        imagePreview: "/thank-you-card-preview.png",
+        sourceCode: "https://github.com/Jbsmall17/thank-you-card-generator",
+        liveServer: "https://jbsmall17.github.io/thank-you-card-generator/",
+        type: "Frontend"
+    
+    }
+]
